778ca1e84959037cd7aa07dffc1b3da8b817b441
rdayok <r.dayok@native.semicolon.africa>
2023-10-10 20:45:05 +0100

    configured a properties file to keep safe keys, urls and other sensitive credentials

---
+++ src/main/java/com/rdi/gemtube/config/MailConfig.java
@@ -0,0 +1,15 @@
+package com.rdi.gemtube.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class MailConfig {
+
+    @Value("${mail.api.key}")
+    private String mailApiKey;
+    @Value("${brevo.mail.url}")
+    private String brevoMailUrl;
+}
